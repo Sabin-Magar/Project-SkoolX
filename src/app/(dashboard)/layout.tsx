d9c4d94acc05ac3dad2b30c09d249ff91452909a
Sabin Magar <alemagarsabin123@gmail.com>
2026-03-17 20:41:58 +0545

    dashboard file created

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <div className="h-screen flex">
+        {/* left contents */}
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">left</div>
+
+        {/* right content */}
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">right</div>
+      </div>
+  );
+}
