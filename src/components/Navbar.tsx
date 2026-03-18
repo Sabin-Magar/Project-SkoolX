2685b40b338e63828a3d65b006c76c35eeb7c44a
Sabin Magar <alemagarsabin123@gmail.com>
2026-03-18 22:08:14 +0545

    navbar elements icons added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,32 @@
+import Image from "next/image"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex items-center justify-between p-4">
+
+      {/* Search bar */}
+      <div className="hidden md:flex">
+        <Image src="/search.png" alt="search" width={14} height={14} />
+        <input type="text" placeholder="Search ...."/>
+      </div>
+
+
+      {/* icons and user */}
+      <div className="flex items-center gap-6 justify-end w-full">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/message.png" alt="" width={20} height={20} />
+        </div>
+
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/announcement.png" alt="" width={20} height={20} />
+        </div>
+
+        <div className="flex flex-col">
+          <span className="">Sabin Magar</span>
+          <span>Admin</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
