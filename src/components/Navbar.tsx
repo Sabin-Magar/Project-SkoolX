bfba175e553e07150081564bf81a4f8dd2b47270
Sabin Magar <alemagarsabin123@gmail.com>
2026-03-18 22:23:05 +0545

    search bar placed in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,10 @@ const Navbar = () => {
     <div className="flex items-center justify-between p-4">
 
       {/* Search bar */}
-      <div className="hidden md:flex">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="search" width={14} height={14} />
-        <input type="text" placeholder="Search ...."/>
+        <input type="text" placeholder="Search ...." className="w-[200px] p-2 bg-transparent outline-none"/>
       </div>
-
 
       {/* icons and user */}
       <div className="flex items-center gap-6 justify-end w-full">
