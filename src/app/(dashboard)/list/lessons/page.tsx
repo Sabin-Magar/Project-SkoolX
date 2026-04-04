ccf27df236c1360c91b93bda729feb4e3630e07c
Sabin Magar <alemagarsabin123@gmail.com>
2026-04-04 20:53:29 +0545

    lesson list page updated

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -46,7 +46,7 @@ const LessonListPage = () => {
 
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/classes/${item.id}`}>
+          <Link href={`/list/lessons/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
               <Image src="/edit.png" alt="" width={16} height={16} />
             </button>
