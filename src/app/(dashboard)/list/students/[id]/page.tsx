72d12e4f27c93299c444db1683f9eb785fa8da8d
Sabin Magar <alemagarsabin123@gmail.com>
2026-04-05 20:34:12 +0545

    created a dynamic form buttons-CRUD

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -18,7 +18,7 @@ const SingleStudentPage = () => {
                     </div>
 
                     <div className="w-2/3 flex flex-col justify-between gap-4">
-                        <h1 className="text-xl font-semibold">Sumana Ranjit</h1>
+                        <h1 className="text-xl font-semibold">Sumana Thapa Magar</h1>
                         <p className="text-sm text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
 
                         <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
