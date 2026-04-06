4372eb5ab4a4f75d49520db93a092df6c1cad90d
Sabin Magar <alemagarsabin123@gmail.com>
2026-04-06 22:37:06 +0545

    rendering forms conditionally

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -50,9 +50,10 @@ const FormModal = ({table,type,data,id}:{
                 <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}?</span>
                 <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
             </form>
+        ) : type === "create" || type === "update" ? (
+            forms[table](type, data)
         ) : (
-            // <TeacherForm type="update" data={data} />
-            <StudentForm type="create" />
+            "Form not found"
         );
     };
 
