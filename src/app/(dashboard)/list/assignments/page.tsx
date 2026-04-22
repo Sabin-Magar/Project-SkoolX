936c26fd0ac6ef87cea346c5bf06c3f99a6d33f7
Sabin Magar <alemagarsabin123@gmail.com>
2026-04-22 21:12:42 +0545

    fetching assignments data

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -3,16 +3,27 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { assignmentsData, role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Assignment, Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Assignment = {
-  id:number;
-  subject:string;
-  class:string;
-  teacher:string;
-  dueDate:string;
-}
+// type Assignment = {
+//   id:number;
+//   subject:string;
+//   class:string;
+//   teacher:string;
+//   dueDate:string;
+// }
+
+type AssignmentList = Assignment & {
+  lesson: {
+    subject: Subject;
+    class: Class;
+    teacher: Teacher;
+  };
+};
 
  const columns = [
     {
@@ -43,14 +54,12 @@ type Assignment = {
       : []),
   ];
 
-const AssignmentListPage = () => {
-
-  const renderRow = (item:Assignment) => (
+  const renderRow = (item:AssignmentList) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
-      <td className="hidden md:table-cell">{item.dueDate}</td>
+      <td className="flex items-center gap-4 p-4">{item.lesson.subject.name}</td>
+      <td>{item.lesson.class.name}</td>
+      <td className="hidden md:table-cell">{item.lesson.teacher.name + " " + item.lesson.teacher.surname}</td>
+      <td className="hidden md:table-cell">{new Intl.DateTimeFormat("en-US").format(item.dueDate)}</td>
 
       <td>
         <div className="flex items-center gap-2">
@@ -65,6 +74,65 @@ const AssignmentListPage = () => {
     </tr>
 );
 
+const AssignmentListPage = async ({
+  searchParams,
+}: {
+  // searchParams: { [key: string]: string  | undefined};
+  searchParams: Promise<{ [key: string]: string  | undefined}>;
+}) => {
+
+  const resolvedParams = await searchParams;
+  // ---------------- use resolvedParams instead of searchParams to avoid hydration issue ----------------
+  // const { page, ...queryParams } = searchParams;
+  const { page, ...queryParams } = resolvedParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // URL Params conditions
+
+  const query: Prisma.AssignmentWhereInput = {};
+
+  query.lesson = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId":
+            query.lesson.classId = parseInt(value);
+            break;
+          case "teacherId":
+            query.lesson.teacherId = value;
+            break;
+          case "search":
+            query.lesson.subject = {
+              name: { contains: value, mode: "insensitive" },
+            };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.assignment.findMany({
+      where: query,
+      include: {
+        lesson: {
+          select: {
+            subject: { select: { name: true } },
+            teacher: { select: { name: true, surname: true } },
+            class: { select: { name: true } },
+          },
+        },
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.assignment.count({ where: query }),
+  ]);
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -88,10 +156,10 @@ const AssignmentListPage = () => {
       </div>
 
       {/* List Section */}
-      <Table columns={columns} renderRow={renderRow} data={assignmentsData}/>
+      <Table columns={columns} renderRow={renderRow} data={data}/>
 
       {/* Pagination Section */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   )
 }
