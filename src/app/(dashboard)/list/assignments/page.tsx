2a8674c0ea4c6bb3a0cddb608c5f8aa124687a59
Sabin Magar <alemagarsabin123@gmail.com>
2026-05-16 19:23:21 +0545

    pages updated

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -488,9 +488,6 @@ const AssignmentListPage = async ({
         </div>
         <div className="flex items-center gap-3">
           <TableSearch />
-          <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-            <Image src="/filter.png" alt="filter" width={14} height={14} />
-          </button>
           {(role === "admin" || role === "teacher") && (
             <FormContainer table="assignment" type="create" />
           )}
--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -562,9 +562,6 @@ const EventListPage = async ({
         </div>
         <div className="flex items-center gap-3">
           <TableSearch />
-          <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-            <Image src="/filter.png" alt="filter" width={14} height={14} />
-          </button>
           {role === "admin" && (
             <FormContainer table="event" type="create" />
           )}
